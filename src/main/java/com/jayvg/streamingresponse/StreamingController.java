15f8b7927897f7ad3fe220f5393786f7c16c6cff
jayvg <jaycloudtechie@gmail.com>
2022-02-08 19:28:07 +0000

    modified business logic

--- src/main/java/com/jayvg/streamingresponse/StreamingController.java
+++ src/main/java/com/jayvg/streamingresponse/StreamingController.java
@@ -13,15 +13,17 @@ public class StreamingController {
     private ExecutorService nonBlockingService = Executors
       .newCachedThreadPool();
     
-    @GetMapping("/sse")
+    @GetMapping("/job")
     public SseEmitter handleSse() {
          SseEmitter emitter = new SseEmitter();
          nonBlockingService.execute(() -> {
              try {
-                 emitter.send("/sse" + " @ " + new Date());
+                 emitter.send("Starting Job execution at " + " @ " + new Date());
                  // sending more events after a sleep to simulate operations 
+                 Thread.currentThread().sleep(5000); // 5 seconds wait ..
+                 emitter.send("Job execution in-progress " + " @ " + new Date());
                  Thread.currentThread().sleep(10000); // 10 seconds wait ..
-                 emitter.send("/sse" + " @ " + new Date());
+                 emitter.send("Job execution completed successfully " + " @ " + new Date());
                  emitter.complete();
              } catch (Exception ex) {
                  emitter.completeWithError(ex);
